adc958ee48f7573671729b1d17e39b0f280ef1b6
DavidGomez00 <dvdgmzrtz@gmail.com>
2025-01-27 11:09:08 +0100

    Organized directories

--- experiment_execution.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for chunk_str in "nat" "nat_sem"; do
-  for model in "BGE-M3" "multilinguale5"; do
-    for llm in "nemotron" "llama3.1:70b-instruct-fp16"; do
-      python main.py --chunk_str "$chunk_str" --model "$model" --llm "$llm"
-    done
-  done
-done
